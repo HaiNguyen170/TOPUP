38a6960f253b54dce3b9a6c6d447920e4e65c842
nminh227 <55972163+nminh227@users.noreply.github.com>
2021-04-01 22:20:13 +0700

    a

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-var express = require('express');
-var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(req, res) {
-    res.send('Hello');
-});
+configViewEngine(app);
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+initWebRoutes(app);
+
+initCronJob();
+
+let port = process.env.PORT || 8080;
+
+app.listen(port, function(){
+    console.log(`App is running at port ${port}`);
   });

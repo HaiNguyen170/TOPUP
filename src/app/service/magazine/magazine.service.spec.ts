c0fde037772dfeea2c9cb786e8d5bb9ea973818d
HaiNguyen170 <57802214+HaiNguyen170@users.noreply.github.com>
2021-04-10 11:07:45 +0700

    Update magazine.service.spec.ts

--- src/app/service/magazine/magazine.service.spec.ts
+++ src/app/service/magazine/magazine.service.spec.ts
@@ -1,16 +1,1 @@
-import { TestBed } from '@angular/core/testing';
 
-import { MagazineService } from './magazine.service';
-
-describe('MagazineService', () => {
-  let service: MagazineService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(MagazineService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
